6a8f1d697768bdb0c6ea9b05392d4df9d7ed400c
Andre LaFleur <cincospenguinos@gmail.com>
2023-11-27 20:38:05 -0700

    Get the movement thing working

--- src/scenes/ForkliftScene/ForkliftScene.js
+++ src/scenes/ForkliftScene/ForkliftScene.js
@@ -2,6 +2,38 @@ import Phaser from 'phaser';
 import { ForkliftStates } from '../../model/Model.js';
 import CONSTANTS from '../../constants';
 
+class Forklift extends Phaser.Physics.Matter.Sprite {
+  constructor(scene, opts) {
+    super(scene.matter.world, opts.x, opts.y, CONSTANTS.keys.forklift);
+    scene.add.existing(this);
+
+    // body, air, static
+    this.setFriction(0.5, 0, 0); // We want to continue indefinitely, until we collide into something
+  }
+
+  stop() {
+    this.setVelocity(0, 0);
+  }
+
+  forward() {
+    this.setVelocity(0, Forklift.MAX_SPEED);
+  }
+
+  backward() {
+    this.setVelocity(0, -Forklift.MAX_SPEED);
+  }
+
+  rotateRight() {
+
+  }
+
+  rotateLeft() {
+
+  }
+}
+
+Forklift.MAX_SPEED = 5;
+
 /**
  * Where the game is actually played.
  */
@@ -15,11 +47,16 @@ export default class ForkliftScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.spritesheet(CONSTANTS.keys.forklift, CONSTANTS.sprites.forklift.location, CONSTANTS.sprites.forklift.config);
+    const { forklift, backlitButtons } = CONSTANTS.sprites;
+
+    this.load.spritesheet(forklift.key, forklift.location, forklift.config);
+    this.load.image(backlitButtons.key, backlitButtons.location);
   }
 
   create() {
-    this.player = this.matter.add.image(500, 500, CONSTANTS.keys.forklift);
+    this.add.tileSprite(0, 0, 10000, 10000, CONSTANTS.keys.backlitButtons);
+
+    this.player = new Forklift(this, { x: 500, y: 500 });
     this.cameras.main.startFollow(this.player);
 
     this.cameras.main.setBackgroundColor('rgba(255, 0, 0, 0.5)');
@@ -36,14 +73,14 @@ export default class ForkliftScene extends Phaser.Scene {
       break;
     case ForkliftStates.MOVEMENT:
       console.log('Movement mode!');
-      this.player.setVelocity(0, 0);
+      this.player.stop();
       break;
     case ForkliftStates.FORWARD:
-      this.player.setVelocity(0, -10);
+      this.player.forward();
       console.log('onwards!');
       break;
     case ForkliftStates.BACKWARD:
-      this.player.setVelocity(0, 10);
+      this.player.backward();
       console.log('backwards!');
       break;
     }
